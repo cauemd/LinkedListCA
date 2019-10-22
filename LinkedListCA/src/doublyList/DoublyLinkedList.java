ef18933c2e82d63aa870f8c31a6e9fb56c9d2f82
Caue Duarte <cauemd91@gmail.com>
2019-10-22 02:32:48 +0100

    LinkedList and Node class created

--- LinkedListCA/src/doublyList/DoublyLinkedList.java
+++ LinkedListCA/src/doublyList/DoublyLinkedList.java
@@ -4,6 +4,12 @@
  */
 package doublyList;
 
-public class DoublyLinkedList {
+public class DoublyLinkedList <E>{
+	
+	private Node first;
+	private Node last;
+	private int size;
+	
+	
 
 }
--- LinkedListCA/src/doublyList/Node.java
+++ LinkedListCA/src/doublyList/Node.java
@@ -4,6 +4,19 @@
  */
 package doublyList;
 
-public class Node {
+import person.Person;
+
+public class Node <E>{
+	
+	private Node next;
+	private Node previous;
+	private E person;
+	
+	//Class constructor, the "next" and "previous" parameters will receive null if the node is the last or first in the list.
+	public Node(Node next, Node previous, E person) {
+		this.next = next;
+		this.previous = previous;
+		this.person = person;
+	}
 
 }
